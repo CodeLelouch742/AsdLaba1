e02bc7a6fe4fee8dc98c0fbead60aabcbebe9955
CodeLelouch742 <114353572+CodeLelouch742@users.noreply.github.com>
2022-11-13 13:55:37 +0300

    Delete laba1 directory

--- laba1/painting/PaintingListUtil.h
+++
@@ -1,8 +0,0 @@
-#ifndef lab1_PAINTINGLISTUTIL_H
-#define lab1_PAINTINGLISTUTIL_H
-
-#include "PaintingJsonUtil.h"
-
-CustomList<Painting>* getIndexPainting(CustomList<Painting>*, int);
-
-#endif //lab1_PAINTINGLISTUTIL_H
